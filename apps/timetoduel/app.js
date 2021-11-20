a5c8aba8135701b94dcbcfc09cd414635c0d863c
Cornell Daly <dalycornell@gmail.com>
2021-11-20 16:22:36 -0500

    Upload to proper app.js files

--- apps/timetoduel/app.js
+++ apps/timetoduel/app.js
@@ -1,12 +1,98 @@
-// place your const, vars, functions or classes here
+const isDarkTheme = g.theme.dark;
+const clearColor = isDarkTheme ? 0x780F : 0x000F;
+const textColor = isDarkTheme ? 0xFD20 : 0xFFE0;
+const hourHandColor = isDarkTheme  ? 0xC618 : 0x780F;
+const minuteHandColor = isDarkTheme  ? 0x7800 : 0x07FF;
 
-// special function to handle display switch on
-Bangle.on('lcdPower', (on) => {
-  if (on) {
-    // call your app function here
-    // If you clear the screen, do Bangle.drawWidgets();
+const hourHandLength = 18;
+const minuteHandLength = 32;
+
+const hourRevolutions = 360 / (60 * 24 / 2);
+const minuteRevolutions = 360 / 60;
+
+function drawBackground() {
+  if (!Bangle.isLCDOn()) {
+    return;
   }
+
+  // g.drawImage(bg, 0, g.getHeight() - bg.height);
+}
+
+function drawFace() {
+  g.setFont('6x8', 2);
+
+  const str = 'Duel';
+  const strWidth = g.stringWidth(str);
+
+  const x = g.getWidth() / 2 - strWidth / 2;
+  const y = 30;
+
+  g.setBgColor(clearColor);
+  g.clearRect(x, y, x + strWidth, y + 24);
+  
+  g.setColor(textColor);
+  g.drawString(str, x, y);
+  g.setRotation(1);
+  g.drawString(str, x, y);
+  g.setRotation(2);
+  g.drawString(str, x, y);
+  g.setRotation(3);
+  g.drawString(str, x, y);
+  g.setRotation(0);
+}
+
+function drawTime() {
+  if (!Bangle.isLCDOn()) {
+    return;
+  }
+
+  const now = new Date();
+  const clockHours = ((now.getHours() % 12) * 60) + now.getMinutes();
+  const clockMinutes = now.getMinutes() + now.getSeconds() / 60;
+
+  const hourAngle = clockHours * hourRevolutions;
+  const minuteAngle = clockMinutes / minuteRevolutions;
+
+  const hourRads = hourAngle * Math.PI / 180;
+  const minuteRads = minuteAngle * Math.PI / 180;
+
+  const x = g.getWidth() / 2;
+  const y = g.getHeight() / 2;
+
+  g.setBgColor(clearColor);
+  g.clearRect(x - 38, y - 38, x + 38, y + 38);
+
+  g.setColor(hourHandColor);
+  g.drawLine(x, y, x - Math.cos(hourRads)*hourHandLength, y - Math.sin(hourRads)*hourHandLength);
+
+  g.setColor(minuteHandColor);
+  g.drawLine(x, y, x - Math.cos(minuteRads)*minuteHandLength, y - Math.sin(minuteRads)*minuteHandLength);
+}
+
+function drawClock() {
+  if (!Bangle.isLCDOn()) {
+    return;
+  }
+
+  drawBackground();
+  drawFace();
+  drawTime();
+}
+
+Bangle.setOptions({
+  wakeOnTouch: true,
 });
 
+g.setBgColor(clearColor);
 g.clear();
-// call your app function here
+
+Bangle.loadWidgets();
+Bangle.drawWidgets();
+
+drawClock();
+
+Bangle.setUI("clockupdown", btn => {
+  drawClock();
+});
+
+const tick = setInterval(drawTime, 1000);
--- apps/vaporwatch/app.js
+++ apps/vaporwatch/app.js
@@ -3,47 +3,19 @@ const storage = require('Storage');
 const settings = (storage.readJSON('setting.json', 1) || {});
 const is12Hour = settings["12hour"] || false;
 
-const isDarkTheme = g.theme.dark;
-const clearColor = isDarkTheme ? 0x780F : 0x000F;
-const textColor = isDarkTheme ? 0xFD20 : 0xFFE0;
-const hourHandColor = isDarkTheme  ? 0xC618 : 0x780F;
-const minuteHandColor = isDarkTheme  ? 0x7800 : 0x07FF;
+const bg = {
+  width : 176, height : 176, bpp : 8,
+  buffer : require("heatshrink").decompress(atob("AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4Ak6gAlM34A/V/4A/VP61/AH6v/V/4A/Vf6z/V/6v/AH4A/AH4A/AH4A/AH4AM5AA9P36v/V/4A/Vn6v/AH6v/WX6t/AH6v/V/6x/AH6v/V/4A/AH4A/AH4A/AH4Aq4QA/ABJ8xgQAxUn6v8gYAxUn4AKf4gAsV/4A9PuSv/V/4Auh4AvUX6x+V/4A/P+Cv/V/4A/V/6v/AH6v/AD0VAH4A/AH4A/AH4A/AH+2AH4ANR36v/V/4A/V/6v/AH6v/AB8hAH4AfUH6v/V/6v/AH6v/V/4A/V/6v/AH6g/V/6v/V/4A/V/6v/AH6v/V/4AO3IA/AH4A/AH4A/AEEAACYWSFKYTQCGRZmCoyv/CH6vvVqhBoV/6FaTS6vnCIKv/e1SvuKfKv/WGqv/EVqv/H1Kv/TTqwoTSgBCV/74rV9Q8pV/6aeWEyv/V/4peAH4A/AH4A/AH4AdgAAPCSQVXCiIR1OrAWBCqiv/CP6vaWEA4rTuxLTV/4VPAISv/fd6wkG1id2V/6v/V/6YcWDo1tV/6v/DrAVBV/78zWEIztTkiv/V/6v/V+CwaGVyv/V/4ZYCwKv/WH4MLAH4A/AH4A/AH4AdgAAJBhgALDCwWSCXB+bF94XBV/7/4V+pepV/6BqFtqv/V/6v/V/6BdDSQssV7LC3HgqDYV9YZXREwTnQbgaQFVSv/V/6v/V/6EkDZwppTf6v/FMAaXV/6wwbGib/V/QbKV/4T/Q9wmbV/6v/DibWmTf4ULNt4bIAoIA/AH4A/AH4A/ADsAgADDADIdXC6gUTFFAXbDpYjcDjCv/N94cIaUav/V/4dLV/6v/V9wgdV/7EsV8IeCaECv/V9qweZzyv/CjSR3V/4U/V/5W8V/6v/HjKEqV9qw8V/6v/V/6v/V/6v/V/6v/HVKv/LV6v/V/6v/V/6vvCoKv2DoIA/AH4A/AH4A/ADsAADIcaDSoV/DUIAcV/6v/ACgA=="))
+};
 
-const hourHandLength = 18;
-const minuteHandLength = 32;
-
-const hourRevolutions = 360 / (60 * 24 / 2);
-const minuteRevolutions = 360 / 60;
+const textColor = g.theme.dark ? '#a0a' : '#080';
 
 function drawBackground() {
   if (!Bangle.isLCDOn()) {
     return;
   }
 
-  // g.drawImage(bg, 0, g.getHeight() - bg.height);
-}
-
-function drawFace() {
-  g.setFont('6x8', 2);
-
-  const str = 'Duel';
-  const strWidth = g.stringWidth(str);
-
-  const x = g.getWidth() / 2 - strWidth / 2;
-  const y = 30;
-
-  g.setBgColor(clearColor);
-  g.clearRect(x, y, x + strWidth, y + 24);
-  
-  g.setColor(textColor);
-  g.drawString(str, x, y);
-  g.setRotation(1);
-  g.drawString(str, x, y);
-  g.setRotation(2);
-  g.drawString(str, x, y);
-  g.setRotation(3);
-  g.drawString(str, x, y);
-  g.setRotation(0);
+  g.drawImage(bg, 0, g.getHeight() - bg.height);
 }
 
 function drawTime() {
@@ -52,26 +24,20 @@ function drawTime() {
   }
 
   const now = new Date();
-  const clockHours = ((now.getHours() % 12) * 60) + now.getMinutes();
-  const clockMinutes = now.getMinutes() + now.getSeconds() / 60;
+  const hours = now.getHours();
+  const clockHours = ('0' + ((is12Hour && hours > 12) ? hours - 12 : hours)).substr(-2);
+  const clockMinutes = ('0' + now.getMinutes()).substr(-2);
 
-  const hourAngle = clockHours * hourRevolutions;
-  const minuteAngle = clockMinutes / minuteRevolutions;
+  const time = `${clockHours}:${clockMinutes}`;
 
-  const hourRads = hourAngle * Math.PI / 180;
-  const minuteRads = minuteAngle * Math.PI / 180;
+  const x = 44;
+  const y = 34;
 
-  const x = g.getWidth() / 2;
-  const y = g.getHeight() / 2;
-
-  g.setBgColor(clearColor);
-  g.clearRect(x - 38, y - 38, x + 38, y + 38);
-
-  g.setColor(hourHandColor);
-  g.drawLine(x, y, x - Math.cos(hourRads)*hourHandLength, y - Math.sin(hourRads)*hourHandLength);
-
-  g.setColor(minuteHandColor);
-  g.drawLine(x, y, x - Math.cos(minuteRads)*minuteHandLength, y - Math.sin(minuteRads)*minuteHandLength);
+  g.setFont('6x8', 3);
+  g.setColor(textColor);
+  g.setBgColor('#000');
+  g.clearRect(x, y, x + ((time.length + 1) * 18), y + 24);
+  g.drawString(time, x, y);
 }
 
 function drawClock() {
@@ -80,15 +46,44 @@ function drawClock() {
   }
 
   drawBackground();
-  drawFace();
   drawTime();
 }
+
+let dragStart = null;
+
+Bangle.on('touch', (zone, e)  => {
+  if (zone === 2) {
+    Bangle.setLCDBrightness(1);
+    Bangle.drawWidgets();
+  }
+});
+
+Bangle.on('drag', e => {
+  const b = e.b;
+
+  if (b === 1 && !dragStart) {
+    dragStart = e;
+  } else if (b === 0 && dragStart) {
+    const sx = dragStart.x;
+    const sy = dragStart.y;
+    const ex = e.x;
+    const ey = e.y;
+    const dx = sx - ex;
+    const dy = sy - ey;
+
+    dragStart = null;
+
+    if (sy < 144 && dy > 14 && Math.abs(dx) < 20) {
+      Bangle.setLCDBrightness(1);
+      Bangle.showLauncher();
+    }
+  }
+});
 
 Bangle.setOptions({
   wakeOnTouch: true,
 });
 
-g.setBgColor(clearColor);
 g.clear();
 
 Bangle.loadWidgets();
@@ -96,8 +91,19 @@ Bangle.drawWidgets();
 
 drawClock();
 
-Bangle.setUI("clockupdown", btn => {
-  drawClock();
+Bangle.on('lcdPower', on => {
+  if (on) {
+    drawClock();
+    Bangle.drawWidgets();
+  }
 });
 
 const tick = setInterval(drawTime, 1000);
+
+setWatch(() => {
+  if (Bangle.isLCDOn()) {
+    Bangle.setLCDBrightness(0);
+  }
+}, BTN, {
+  repeat: true,
+});
